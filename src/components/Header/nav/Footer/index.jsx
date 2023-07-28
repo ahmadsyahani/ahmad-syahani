0c62e95cef036c9b8fda3259c8f965bbf118874e
ahmadsyahani <10ahmadsyahanirpl@gmail.com>
2023-07-28 20:41:05 +0700

    sidebar-style-change

--- src/components/Header/nav/Footer/index.jsx
+++ src/components/Header/nav/Footer/index.jsx
@@ -1,11 +1,27 @@
 import styles from './style.module.scss';
+import Magnetic from '/src/common/Magnetic';
 
 export default function index() {
   return (
     <div className={styles.footer}>
-      <a>Instagram</a>
-      <a>Youtube</a>
-      <a>Tiktok</a>
+      <Magnetic>
+        <div className={styles.el}>
+          <a>Youtube</a>
+          <div className={styles.indicator}></div>
+        </div>
+      </Magnetic>
+      <Magnetic>
+        <div className={styles.el}>
+          <a>Instagram</a>
+          <div className={styles.indicator}></div>
+        </div>
+      </Magnetic>
+      <Magnetic>
+        <div className={styles.el}>
+          <a>Tiktok</a>
+          <div className={styles.indicator}></div>
+        </div>
+      </Magnetic>
     </div>
   )
 }
